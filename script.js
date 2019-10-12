b886ded126e72ebb3dab1ffaea988bf928358d2f
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-12 15:01:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ btn9AM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea9AM").value;
     	document.getElementById("demo").innerHTML = x;
-	localStorage.setItem("textarea9AM", x);
+	window.localStorage.setItem("textarea9AM", x);
 })
 
 btn10AM.addEventListener('click', function()
@@ -67,26 +67,26 @@ loadReminders();
 
 function storeReminders()
 {
-	localStorage.setItem("textarea9AM", document.getElementById("textarea9AM").value);
-	localStorage.setItem("textarea10AM", document.getElementById("textarea10AM").value);
-	localStorage.setItem("textarea11AM", document.getElementById("textarea11AM").value);
-   	localStorage.setItem("textarea12PM", document.getElementById("textarea12PM").value);
-   	localStorage.setItem("textarea1PM", document.getElementById("textarea1PM").value);
-   	localStorage.setItem("textarea2PM", document.getElementById("textarea2PM").value);
-   	localStorage.setItem("textarea3PM", document.getElementById("textarea3PM").value);
-   	localStorage.setItem("textarea4PM", document.getElementById("textarea4PM").value);
-   	localStorage.setItem("textarea5PM", document.getElementById("textarea5PM").value);
+	window.localStorage.setItem("textarea9AM", document.getElementById("textarea9AM").value);
+	window.localStorage.setItem("textarea10AM", document.getElementById("textarea10AM").value);
+	window.localStorage.setItem("textarea11AM", document.getElementById("textarea11AM").value);
+   	window.localStorage.setItem("textarea12PM", document.getElementById("textarea12PM").value);
+   	window.localStorage.setItem("textarea1PM", document.getElementById("textarea1PM").value);
+   	window.localStorage.setItem("textarea2PM", document.getElementById("textarea2PM").value);
+   	window.localStorage.setItem("textarea3PM", document.getElementById("textarea3PM").value);
+   	window.localStorage.setItem("textarea4PM", document.getElementById("textarea4PM").value);
+   	window.localStorage.setItem("textarea5PM", document.getElementById("textarea5PM").value);
 }
 
 function loadReminders() 
 {
-    document.getElementById("textarea9AM").value = localStorage.getItem("textarea9AM");
-    document.getElementById("textarea10AM").value = localStorage.getItem("textarea10AM");
-    document.getElementById("textarea11AM").value = localStorage.getItem("textarea11AM");
-    document.getElementById("textarea12PM").value = localStorage.getItem("textarea12PM");
-    document.getElementById("textarea1PM").value = localStorage.getItem("textarea1PM");
-    document.getElementById("textarea2PM").value = localStorage.getItem("textarea2PM");
-    document.getElementById("textarea3PM").value = localStorage.getItem("textarea3PM");
-    document.getElementById("textarea4PM").value = localStorage.getItem("textarea4PM");
-    document.getElementById("textarea5PM").value = localStorage.getItem("textarea5PM");
+    document.getElementById("textarea9AM").value = window.localStorage.getItem("textarea9AM");
+    document.getElementById("textarea10AM").value = window.localStorage.getItem("textarea10AM");
+    document.getElementById("textarea11AM").value = window.localStorage.getItem("textarea11AM");
+    document.getElementById("textarea12PM").value = window.localStorage.getItem("textarea12PM");
+    document.getElementById("textarea1PM").value = window.localStorage.getItem("textarea1PM");
+    document.getElementById("textarea2PM").value = window.localStorage.getItem("textarea2PM");
+    document.getElementById("textarea3PM").value = window.localStorage.getItem("textarea3PM");
+    document.getElementById("textarea4PM").value = window.localStorage.getItem("textarea4PM");
+    document.getElementById("textarea5PM").value = window.localStorage.getItem("textarea5PM");
 }
