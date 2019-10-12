6f31d6cd4c9a9d0a58225a8e82c23b9630a8a1f6
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-12 14:42:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,32 @@ btn5PM.addEventListener('click', function()
 	x = document.getElementById("textarea5PM").value;
     	document.getElementById("demo").innerHTML = x;
 })
+
+storeReminders();
+loadReminders();
+
+function storeReminders()
+{
+	localStorage.setItem("textarea9AM", document.getElementById("textarea9AM").value);
+	localStorage.setItem("textarea10AM", document.getElementById("textarea10AM").value);
+	localStorage.setItem("textarea11AM", document.getElementById("textarea11AM").value);
+   	localStorage.setItem("textarea12PM", document.getElementById("textarea12PM").value);
+   	localStorage.setItem("textarea1PM", document.getElementById("textarea1PM").value);
+   	localStorage.setItem("textarea2PM", document.getElementById("textarea2PM").value);
+   	localStorage.setItem("textarea3PM", document.getElementById("textarea3PM").value);
+   	localStorage.setItem("textarea4PM", document.getElementById("textarea4PM").value);
+   	localStorage.setItem("textarea5PM", document.getElementById("textarea5PM").value);
+}
+
+function loadReminders() 
+{
+    document.getElementById("textarea9AM").value = localStorage.getItem("textarea9AM");
+    document.getElementById("textarea10AM").value = localStorage.getItem("textarea10AM");
+    document.getElementById("textarea11AM").value = localStorage.getItem("textarea11AM");
+    document.getElementById("textarea12PM").value = localStorage.getItem("textarea12PM");
+    document.getElementById("textarea1PM").value = localStorage.getItem("textarea1PM");
+    document.getElementById("textarea2PM").value = localStorage.getItem("textarea2PM");
+    document.getElementById("textarea3PM").value = localStorage.getItem("textarea3PM");
+    document.getElementById("textarea4PM").value = localStorage.getItem("textarea4PM");
+    document.getElementById("textarea5PM").value = localStorage.getItem("textarea5PM");
+}
