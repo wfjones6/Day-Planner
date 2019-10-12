67cd2834e6f4c9d811fe909470168295856d3251
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-12 18:56:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,71 @@
 document.getElementById("demo").innerHTML = "Demo start";
 document.getElementById("demo2").innerHTML = "Demo2 start";
 
+initialLoad();
+function initialLoad()
+{
+	var getVal;
+	getVal = localStorage.getItem("9AM");
+	if (getVal)
+	{
+		document.getElementById("textarea9AM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("10AM");
+	if (getVal)
+	{
+		document.getElementById("textarea10AM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("11AM");
+	if (getVal)
+	{
+		document.getElementById("textarea11AM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("12PM");
+	if (getVal)
+	{
+		document.getElementById("textarea12PM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("1PM");
+	if (getVal)
+	{
+		document.getElementById("textarea1PM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("2PM");
+	if (getVal)
+	{
+		document.getElementById("textarea2PM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("3PM");
+	if (getVal)
+	{
+		document.getElementById("textarea3PM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("4PM");
+	if (getVal)
+	{
+		document.getElementById("textarea4PM").value = getVal;
+	}
+
+	getVal = localStorage.getItem("5PM");
+	if (getVal)
+	{
+		document.getElementById("textarea5PM").value = getVal;
+	}
+}
+
 var x;
 btn9AM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea9AM").value;
     	document.getElementById("demo").innerHTML = x;
+	localStorage.setItem("9AM", x);
 })
 
 btn10AM.addEventListener('click', function()
