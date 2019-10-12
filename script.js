a28b5a061036f71e2c5e9ab9a66d8906aa74862e
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-12 18:36:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
 document.getElementById("demo").innerHTML = "Demo start";
 document.getElementById("demo2").innerHTML = "Demo2 start";
 
+var x;
+btn9AM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea9AM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn10AM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea10AM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn11AM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea11AM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn12PM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea12PM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn1PM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea1PM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn2PM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea2PM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn3PM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea3PM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn4PM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea4PM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
+btn5PM.addEventListener('click', function()
+{
+	x = document.getElementById("textarea5PM").value;
+    	document.getElementById("demo").innerHTML = x;
+})
+
