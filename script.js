c47edad3c184de774917bbcc53fd250583fcce3a
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-12 14:17:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,53 +2,54 @@ var x;
 btn9AM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea9AM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
+	localStorage.setItem("textarea9AM", x);
 })
 
 btn10AM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea10AM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
 
 btn11AM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea11AM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
 
 btn12PM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea12PM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
 
 btn1PM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea1PM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
 
 btn2PM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea2PM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
 
 btn3PM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea3PM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
 
 btn4PM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea4PM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
 
 btn5PM.addEventListener('click', function()
 {
 	x = document.getElementById("textarea5PM").value;
-    document.getElementById("demo").innerHTML = x;
+    	document.getElementById("demo").innerHTML = x;
 })
