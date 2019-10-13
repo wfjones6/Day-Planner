93ef7a73d38c0c5cd243678f2794fe31f8a177f5
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-12 20:18:48 -0500

    Update timeBlocks.js

--- timeBlocks.js
+++ timeBlocks.js
@@ -1,38 +1,29 @@
 var timeBlocks = [
     {
         title: "9AM",
-        reminder: ""
     },
     {
         title: "10AM",
-        reminder: ""
     },
     {
         title: "11AM",
-        reminder: ""
     },
     {
         title: "12PM",
-        reminder: ""
     },
     {
         title: "1PM",
-        reminder: ""
     },
     {
         title: "2PM",
-        reminder: ""
     },
     {
         title: "3PM",
-        reminder: ""
     },
     {
         title: "4PM",
-        reminder: ""
     },
     {
         title: "5PM",
-        reminder: ""
     }
 ];
